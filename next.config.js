7e1d70fb155aa394212fa445169236372457be19
tectumor <72674429+tectumor@users.noreply.github.com>
2023-12-27 20:36:23 -0600

    add output export to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   env: {
     EMAIL_USER: process.env.EMAIL_USER,
     EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
   },
+  // Optional configurations:
+  // trailingSlash: true,
+  // skipTrailingSlashRedirect: true,
+  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
